c4efcb25aae2f852d776105075e41c0294792465
apahie <apahie0918@gmail.com>
2022-07-15 00:22:08 +0900

    ch3 done

--- src/main/kotlin/money/Dollar.kt
+++ src/main/kotlin/money/Dollar.kt
@@ -2,4 +2,5 @@ package money
 
 class Dollar(var amount: Int) {
     fun times(multiplier: Int) = Dollar(amount * multiplier)
+    override fun equals(other: Any?): Boolean = amount == (other as? Dollar)?.amount
 }
--- todo.md
+++ todo.md
@@ -4,3 +4,7 @@
 - [ ] amount を private にする
 - [x] Dollar の副作用どうする？
 - [ ] Money の丸め処理どうする？
+- [x] equals()
+- [ ] hashCode()
+- [x] null との等価性比較
+- [x] 他のオブジェクトとの等価性比較
