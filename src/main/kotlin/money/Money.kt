db9091900767f7e7fcc68e48f536308d9a9b39ea
apahie <apahie0918@gmail.com>
2022-07-17 15:09:15 +0900

    ch8 done

--- src/main/kotlin/money/Money.kt
+++ src/main/kotlin/money/Money.kt
@@ -1,5 +1,11 @@
 package money
 
-open class Money(internal val amount: Int) {
-    override fun equals(other: Any?): Boolean = other is Money && amount == other.amount && this::class == other::class
+abstract class Money(protected val amount: Int) {
+    abstract fun times(multiplier: Int) : Money
+    override fun equals(other: Any?) : Boolean = other is Money && amount == other.amount && this::class == other::class
+
+    companion object {
+        fun dollar(amount: Int) : Money = Dollar(amount)
+        fun franc(amount: Int) : Money = Franc(amount)
+    }
 }
