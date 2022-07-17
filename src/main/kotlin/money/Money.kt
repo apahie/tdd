358b8c5eac357a64e3d4a692a13f43dbcacb6ee8
apahie <apahie0918@gmail.com>
2022-07-17 23:15:43 +0900

    ch10 done

--- src/main/kotlin/money/Money.kt
+++ src/main/kotlin/money/Money.kt
@@ -1,12 +1,13 @@
 package money
 
-abstract class Money(protected val amount: Int, private val currency: String) {
-    abstract fun times(multiplier: Int) : Money
+open class Money(protected val amount: Int, private val currency: String) {
+    open fun times(multiplier: Int) : Money = Money(amount * multiplier, currency)
     fun currency() : String = currency
-    override fun equals(other: Any?) : Boolean = other is Money && amount == other.amount && this::class == other::class
+    override fun equals(other: Any?) : Boolean = other is Money && amount == other.amount && currency() == other.currency()
 
     companion object {
         fun dollar(amount: Int) : Money = Dollar(amount, "USD")
         fun franc(amount: Int) : Money = Franc(amount, "CHF")
     }
+    override fun toString(): String = "$amount $currency"
 }
